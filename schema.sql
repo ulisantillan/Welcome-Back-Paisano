ed55528b594c1ddee5b2990f493b3f9d9d2e2701
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2018-09-06 14:08:33 -0500

    schema updated, additional table, data and additional values to the existing table

--- schema.sql
+++ schema.sql
@@ -36,6 +36,7 @@ INSERT INTO teamMembers (photo, name, about) VALUES ("4kErWao", "Brian Ramirez",
 
 
 
+
 /*  Execute this file from the command line by typing:
 *    mysql -u root < server/schema.sql
 *  to create the database and the tables.
